9bb76f2153d2fc78fca2742e7c0e6faa32489d61
Abu Sadeed <abusadeed@iut-dhaka.edu>
2021-03-18 15:49:27 +0600

    Vehicles can now have types

--- vehicle_showroom.py
+++ vehicle_showroom.py
@@ -4,25 +4,39 @@ print('Vehicle Management')
 
 class Vehicle_Insertion:
     def __init__(self):
+        self._type = ''
         self._model = ''
         self._engine = ''
         self._power = 0
         self._tire = 0
 
-    # Adding normal vehicle
-    def create_normal_vehicle(self):
+    # Adding vehicle
+    def create_vehicle(self, choice):
+
+        # Assigning the type of vehicle
+        if choice == '1':
+            vehicle_type = 'Normal Vehicle'
+        elif choice == '2':
+            vehicle_type = 'Sports Car'
+        elif choice == '3':
+            vehicle_type = 'Heavy Vehicle'
+        else:
+            print('Wrong Vehicle Selection')
+
         try:
+            self._type = vehicle_type
             self._model = input('Enter Vehicle Model Number: ')
             self._engine = input('Enter Engine Type: ')
             self._power = int(input('Enter Engine Power: '))
             self._tire = int(input('Enter Tire Size: '))
             return True
-        except Error:
+
+        except Exception:
             print('Please Enter Accurate Vehicle Information')
             return False
 
     def __str__(self):
-        return '\t'.join(str(a) for a in [self._model, self._engine, self._power, self._tire])
+        return '\t'.join(str(a) for a in [self._type, self._model, self._engine, self._power, self._tire])
 
 
 # Vehicle Management Main
@@ -30,15 +44,15 @@ class Vehicle_Management:
     def __init__(self):
         self.vehicles = []
 
-    def add_vehicle(self):
+    def add_vehicle(self, choice):
         vehicle = Vehicle_Insertion()
-        if vehicle.create_normal_vehicle() == True:
+        if vehicle.create_vehicle(choice) == True:
             self.vehicles.append(vehicle)
             print()
             print('The Vehicle Data Has Been Successfully Inserted!')
 
     def vehicle_display_panel(self):
-        print('\t'.join(['', 'Model', 'Engine', 'Power', 'Tire']))
+        print('\t'.join(['', 'Type', '', 'Model', 'Engine', 'Power', 'Tire']))
         for idx, vehicle in enumerate(self.vehicles):
             print(idx + 1, end='\t')
             print(vehicle)
@@ -57,7 +71,13 @@ while True:
 
     # Adding Vehicle
     if user_choice == '1':
-        inventory.add_vehicle()
+        # User choice for vehicle choice
+        print('Choice 1: Add A Normal Vehicle')
+        print('Choice 2: Add A Sports Vehicle')
+        print('Choice 3: Add A Heavy Vehicle')
+        vehicle_choice = input('Please Choose The Vehicle You Wish To Enter')
+
+        inventory.add_vehicle(vehicle_choice)
 
         # Viewing inventory records
     elif user_choice == '2':
